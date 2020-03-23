4a7901e823aaf116c7921a22b3216416756414ac
Ezekiel Clark <clarkzeke@gmail.com>
2020-03-22 20:25:48 -0600

    final commit

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   scope :filter_by_name, -> (name) { where("lower(name) like ?", "%#{name.downcase}%")}
   scope :filter_by_created_at, -> (created_at) { where("created_at = ?", "%#{created_at.upcase}%")}
   scope :filter_by_updated_at, -> (updated_at) { where("updated_at = ?", "%#{updated_at.upcase}%")}
+
+  def revenue
+    
+  end
 end
